43f86d65951343adf4ea4db65600c312638dfacd
OsamaAlhejaily <osamahhumdan@gmail.com>
2025-04-09 12:14:54 +0300

    removed unnecessary lines of code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { LoginComponent } from './components/auth/login/login.component'
 import { RegisterComponent } from './components/auth/register/register.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { DoctorDashboardComponent } from './components/doctor/doctor-dashboard.component'
-// import { DoctorProfileComponent } from './components/doctor/doctor-profile.component'
 import { UpcomingAppointmentsComponent } from './components/doctor/upcoming-appointments.component'
-// import { PastAppointmentsComponent } from './components/doctor/past-appointments.component'
 
 export const routes: Routes = [
   {
@@ -14,9 +12,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DoctorDashboardComponent },
-      // { path: 'profile', component: DoctorProfileComponent },
       { path: 'upcoming-appointments', component: UpcomingAppointmentsComponent },
-      // { path: 'past-appointments', component: PastAppointmentsComponent }
     ],
   },
   {
